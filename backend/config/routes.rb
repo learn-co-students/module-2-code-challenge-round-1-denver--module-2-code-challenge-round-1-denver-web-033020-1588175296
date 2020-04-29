ee16834fff24fa4d5364055665e69408d74150ae
indyindiana <indiana.danaher@gmail>
2020-04-29 14:26:03 -0600

    mod 2 cahllenge 1

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :powers
+  resources :heroines, only: [:index, :create, :show ]
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
